f4c9a5f5dce29cccf06fa9fda246477a5cb42a26
saullocastro <saullogiovani@gmail.com>
2018-06-19 14:15:47 +0200

    BUG: fixed Newton-Raphson, was storing the wrong load factor per step

--- structsolve/newton_raphson.py
+++ structsolve/newton_raphson.py
@@ -4,108 +4,96 @@ from .logger import msg, warn
 from .static import solve
 
 
-def _solver_NR(run, silent=False):
+def _solver_NR(an, silent=False, initialInc=None):
     """Newton-Raphson solver
 
     """
-    msg('Initialization...', level=1, silent=silent)
+    msg('Initializing...', level=1, silent=silent)
 
-    modified_NR = run.modified_NR
-    inc = run.initialInc
+    modified_NR = an.modified_NR
+    if initialInc is None:
+        initialInc = an.initialInc
+    inc = initialInc
     total = inc
     once_at_total = False
     max_total = 0.
 
-    fext = run.calc_fext(inc=inc, silent=silent)
-    kC = run.calc_kC(silent=silent)
-    c = solve(kC, fext, silent=silent)
-    kT_last = kC
-
-    if modified_NR:
-        compute_kT = False
-    else:
-        compute_kT = True
+    fext = an.calc_fext(inc=1., silent=True)
+    kC = an.calc_kC(silent=silent)
+    c = solve(kC, total*fext, silent=True)
+    kC = an.calc_kC(c=c, NLgeom=True, silent=True)
+    kG = an.calc_kG(c=c, NLgeom=True, silent=True)
+    kT = kC + kG
+    dc = 0 * c
+    c_last = 0 * c
+    compute_NL_matrices = False
 
     step_num = 1
+
     while True:
-        msg('Started Load Step {} - '.format(step_num)
-            + 'Attempting time = {0}'.format(total), level=1, silent=silent)
-
-        # TODO maybe for pdC=True, pdT the fext must be calculated with
-        # the last kT available...
-
-        absERR = 1.e6
-        relERR = 1.e6
+        msg('Step %d, attempting time %1.5f' % (step_num, total), level=1, silent=silent)
         min_Rmax = 1.e6
         prev_Rmax = 1.e6
-        last_min_Rmax = 1.e6
-        iteration = 0
         converged = False
-
-        kT = kT_last
-
-        fext = run.calc_fext(inc=total, silent=silent)
+        iteration = 0
 
         iter_NR = 0
         while True:
             iteration += 1
             msg('Iteration: {}'.format(iteration), level=2, silent=silent)
-            if iteration > run.maxNumIter:
+            if iteration > an.maxNumIter:
                 warn('Maximum number of iterations achieved!', level=2, silent=silent)
                 break
 
-            if compute_kT or (run.kT_initial_state and step_num == 1 and
-                    iteration == 1) or iter_NR == (run.compute_every_n - 1):
+            if compute_NL_matrices or (an.kT_initial_state and step_num == 1 and
+                    iteration == 1) or iter_NR == (an.compute_every_n - 1):
                 iter_NR = 0
-                kC = run.calc_kC(c=c*0, silent=silent, NLgeom=True)
-                kG = run.calc_kG(c=c*0, silent=silent, NLgeom=True)
+                kC = an.calc_kC(c=(c + dc), NLgeom=True, silent=True)
+                kG = an.calc_kG(c=(c + dc), NLgeom=True, silent=True)
                 kT = kC + kG
+                compute_NL_matrices = False
             else:
                 iter_NR += 1
                 if not modified_NR:
-                    compute_kT = True
+                    compute_NL_matrices = True
 
-            fint = run.calc_fint(c=c, silent=silent)
-
-            R = fext - fint
+            fint = an.calc_fint(c=(c + dc), silent=True)
+            R = total*fext - fint
 
             # convergence criteria:
             # - maximum residual force Rmax
             Rmax = np.abs(R).max()
-            msg('Rmax = {0}'.format(Rmax), level=3, silent=silent)
 
-            if iteration >= 2 and Rmax < run.absTOL:
+            if iteration >= 2 and Rmax < an.absTOL:
                 converged = True
                 break
-            if (Rmax > prev_Rmax and Rmax > min_Rmax and iteration > 2):
-                warn('Diverged!', level=2, silent=silent)
+            if (Rmax > prev_Rmax and Rmax > min_Rmax and iteration > 3):
+                warn('Diverged - Rmax value significantly increased', level=2, silent=silent)
                 break
             else:
                 min_Rmax = min(min_Rmax, Rmax)
-            change_rate_Rmax = abs(prev_Rmax-Rmax)/abs(prev_Rmax)
-            if (iteration > 2 and change_rate_Rmax < run.too_slow_TOL):
-                warn('Diverged! (convergence too slow)', level=2, silent=silent)
+            change_rate_Rmax = abs(1 - Rmax/prev_Rmax)
+            if (iteration > 2 and change_rate_Rmax < an.too_slow_TOL):
+                warn('Diverged - convergence too slow', level=2, silent=silent)
                 break
             prev_Rmax = Rmax
 
-            msg('Solving... ', level=2, silent=silent)
-            delta_c = solve(kT, R, silent=silent)
-            msg('finished!', level=2, silent=silent)
+            varc = solve(kT, R, silent=True)
 
             eta1 = 0.
             eta2 = 1.
-            if run.line_search:
+            if an.line_search:
                 msg('Performing line-search... ', level=2, silent=silent)
                 iter_line_search = 0
                 while True:
-                    c1 = c + eta1*delta_c
-                    c2 = c + eta2*delta_c
-                    fint1 = run.calc_fint(c=c1, silent=silent)
-                    fint2 = run.calc_fint(c=c2, silent=silent)
-                    R1 = fext - fint1
-                    R2 = fext - fint2
-                    s1 = delta_c.dot(R1)
-                    s2 = delta_c.dot(R2)
+                    c1 = c + dc + eta1*varc
+                    c2 = c + dc + eta2*varc
+                    fint1 = an.calc_fint(c=c1, silent=True)
+                    fint2 = an.calc_fint(c=c2, silent=True)
+                    R1 = total*fext - fint1
+                    R2 = total*fext - fint2
+                    s1 = varc.dot(R1)
+                    s2 = varc.dot(R2)
                     eta_new = (eta2-eta1)*(-s1/(s2-s1)) + eta1
                     eta1 = eta2
                     eta2 = eta_new
@@ -113,54 +101,46 @@ def _solver_NR(run, silent=False):
                     if abs(eta2-eta1) < 0.01:
                         break
                     iter_line_search += 1
-                    if iter_line_search == run.max_iter_line_search:
+                    if iter_line_search == an.max_iter_line_search:
                         eta2 = 1.
                         warn('maxinum number of iterations', level=3, silent=silent)
                         break
-                msg('finished!', level=2, silent=silent)
-            c = c + eta2*delta_c
+                msg('finished line-search', level=2, silent=silent)
+            dc = dc + eta2*varc
 
         if converged:
-            msg('Finished Load Step {} at'.format(step_num)
-                + ' time = {0}'.format(total), level=1, silent=silent)
-            run.increments.append(total)
-            run.cs.append(c.copy()) #NOTE copy required
+            msg('Converged at time %1.5f' % total, level=2, silent=silent)
+            c_last = c.copy()
+            c = c + dc
+            an.cs.append(c.copy()) #NOTE copy required
+            an.increments.append(total)
+            total += inc
             finished = False
             if abs(total - 1) < 1e-3:
                 finished = True
             else:
-                factor = 1.1
+                factor = 1.1111
                 if once_at_total:
-                    inc_new = min(factor*inc, run.maxInc, (1.-total)/2)
+                    inc_new = min(factor*inc, an.maxInc, (1.-total)/2)
                 else:
-                    inc_new = min(factor*inc, run.maxInc, 1.-total)
-                msg('Changing time increment from {0:1.9f} to {1:1.9f}'.format(
-                    inc, inc_new), level=1, silent=silent)
+                    inc_new = min(factor*inc, an.maxInc, 1.-total)
                 inc = inc_new
                 total += inc
                 total = min(1, total)
                 step_num += 1
             if finished:
                 break
-            if modified_NR:
-                msg('Updating kT...', level=1, silent=silent)
-                kC = run.calc_kC(c=c, silent=silent, NLgeom=True)
-                kG = run.calc_kG(c=c, silent=silent, NLgeom=True)
-                kT = kC + kG
-                msg('kT updated!', level=1, silent=silent)
-            compute_kT = False
-            kT_last = kT
+
         else:
             max_total = max(max_total, total)
             while True:
-                factor = 0.3
-                msg('Bisecting time increment from {0} to {1}'.format(
-                    inc, inc*factor), level=1, silent=silent)
+                factor = 0.9
+                msg('Reseting step with reduced time increment', level=1, silent=silent)
                 if abs(total - 1) < 1e-3:
                     once_at_total = True
                 total -= inc
                 inc *= factor
-                if inc < run.minInc:
+                if inc < an.minInc:
                     msg('Minimum step size achieved!', level=1, silent=silent)
                     break
                 total += inc
@@ -168,18 +148,18 @@ def _solver_NR(run, silent=False):
                     continue
                 else:
                     break
-            if inc < run.minInc:
-                msg('Stopping solver: minimum step size achieved!',
-                        level=1, silent=silent)
-                break
 
-        if len(run.cs)>0:
-            c = run.cs[-1].copy() #NOTE copy required
-        else:
-            # means that run bisection must be done in initialInc
-            fext = run.calc_fext(inc=inc, silent=silent)
-            c = solve(kC, fext, silent=silent)
+        if inc < an.minInc:
+            msg('Minimum step size of %1.5f achieved!' % an.minInc, level=1, silent=silent)
+            break
+
+        dc = c - c_last
+
+        kC = an.calc_kC(c=c, NLgeom=True, silent=True)
+        kG = an.calc_kG(c=c, NLgeom=True, silent=True)
+        kT = kC + kG
+        compute_NL_matrices = False
 
     msg('Finished Non-Linear Static Analysis', silent=silent)
-    msg('at time {0}'.format(total), level=1, silent=silent)
+    msg('    total time %1.5f' % total, level=1, silent=silent)
 
